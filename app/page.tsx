d0179ddda0033bf1f278dff72067c44252fe3a29
Asif <txxasif@gmail.com>
2024-09-01 22:32:10 +0600

    added react query

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import Images from "@/components/Images";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { getImages } from "@/helper/api";
-export default async function Home() {
-  const result = await getImages();
-  if (!result) {
+export default function Home() {
+  const { data: result = [], isPending } = useQuery({
+    queryKey: ["images"],
+    queryFn: getImages,
+  });
+  if (isPending) {
     return null;
   }
+  console.log(result);
 
   return (
     <main className="mx-auto max-w-[1960px] p-4">
--- helper/api.ts
+++ helper/api.ts
@@ -9,18 +9,18 @@ export async function uploadImages(data: Image[] | undefined) {
   console.log(`${host}/api/upload/images`);
   console.log(process.env.LOCAL_URL);
 
-  return await axios.post(`${host}/api/upload/images`, data);
+  return await axiosInstance.post(`${host}/api/upload/images`, data);
 }
 
 export async function getImages(): Promise<ImageProps[]> {
-  return await axios
+  return await axiosInstance
     .get(`${host}/api/upload/images`)
     .then((res) => res.data.data);
 }
 export async function getImageById(id: number): Promise<ImageProps> {
   console.log(id);
 
-  const data = await axios
+  const data = await axiosInstance
     .get(`${host}/api/image?pid=${id}`)
     .then((res) => res.data.data);
   console.log(data);
