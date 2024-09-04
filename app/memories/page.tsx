b7f1ed21e925e63a67eb41457ec0f01439e55489
Asif <txxasif@gmail.com>
2024-09-04 14:33:14 +0600

    memories

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -1,15 +1,18 @@
+"use client";
 import React from "react";
 import { FilleManager } from "@/components/FilleManager";
-
+import BlurFade from "@/components/magicui/blur-fade";
 export default function Memories() {
   return (
-    <main className="w-full min-h-screen flex flex-col items-center justify-center  px-4">
+    <main className="w-full min-h-[80vh] flex flex-col items-center justify-center  px-4">
       <div className="w-full flex justify-center mb-8">
         <h1 className="font-mono text-white text-5xl md:text-6xl lg:text-7xl text-center">
           File Manager
         </h1>
       </div>
+
       <div className="w-full max-w-4xl flex items-center justify-center  shadow-lg rounded-lg p-4 md:p-8">
+        {" "}
         <FilleManager />
       </div>
     </main>
