bfcd549a9f7b8320fed50fbf473e6ba146816ea7
Asif <txxasif@gmail.com>
2024-09-05 23:42:20 +0600

    memories

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
+"use client";
 import { Notifications } from "@/components/ui/Notification";
 import PhotoSection from "@/components/PhotoSection";
 import WelcomeMessage from "@/components/WelcomeMessage";
 import { Gallery } from "@/components/mock";
+import React, { useRef } from "react";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function About() {
   return (
     <main className=" flex flex-col min-h-screen   ">
-      <WelcomeMessage />
-      <Gallery />
-      <PhotoSection />
-      <Notifications />
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.05,
+        }}
+      >
+        <WelcomeMessage />
+        <Gallery />
+        <PhotoSection />
+        <Notifications />
+      </ReactLenis>
     </main>
   );
 }
